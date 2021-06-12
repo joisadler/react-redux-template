4787761da984df07e1b3411c5e9da87cb7327bcd
Yossi Adler <joisadler@gmail.com>
2021-06-12 09:29:09 +0300

    add header links and styles

--- src/modules/common/header/Header.jsx
+++ src/modules/common/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import styles from './header.module.scss';
 
 const Header = () => {
-  return <header className={styles.container}>header</header>;
+  return (
+    <header className={styles.container}>
+      <nav>
+        <NavLink
+          exact
+          to="/"
+          className={styles.link}
+          activeClassName={styles.link_active}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          exact
+          to="/entities"
+          className={styles.link}
+          activeClassName={styles.link_active}
+        >
+          Entities
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
