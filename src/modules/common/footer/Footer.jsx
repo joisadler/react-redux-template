8b2a69b933762827930f62c0926ae347b38323e1
Yossi Adler <joisadler@gmail.com>
2021-06-12 18:48:15 +0300

    add a year to the footer

--- src/modules/common/footer/Footer.jsx
+++ src/modules/common/footer/Footer.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import styles from './footer.module.scss';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.container}>
       <p className={styles.text}>
-        &copy;{' '}
+        &copy; {currentYear}{' '}
         <a href="https://github.com/joisadler" className={styles.link}>
           Yossi Adler
         </a>
